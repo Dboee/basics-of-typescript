73aefb35e0704aa84421ae915e80a472753d790c
Dboee <davidboee@gmail.com>
2022-12-27 15:57:20 +0100

    adds conditional based on completed

--- fetchJson/index.ts
+++ fetchJson/index.ts
@@ -3,21 +3,22 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 interface Todo {
+  userId?: number;
   id: number;
   title: string;
   completed: boolean;
 }
 
 axios.get(url).then((response) => {
-  const todo = response.data;
+  const todo = response.data as Todo;
   console.log('todo: ', todo);
   const ID = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
   console.log(`
 	The todo with ID: ${ID}
 	Had a title of: ${title}
-	It is ${finished}
+	${completed ? 'It is completed' : 'It is not completed'}
 	`);
 });
