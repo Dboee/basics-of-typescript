1e6fad57cb0e49792c7b0d906c761b9350603b93
Dboee <davidboee@gmail.com>
2022-12-27 15:52:57 +0100

    adds basic interface an compiler config

--- fetchJson/index.ts
+++ fetchJson/index.ts
@@ -1,3 +1,23 @@
 import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data;
+  console.log('todo: ', todo);
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+
+  console.log(`
+	The todo with ID: ${ID}
+	Had a title of: ${title}
+	It is ${finished}
+	`);
+});
